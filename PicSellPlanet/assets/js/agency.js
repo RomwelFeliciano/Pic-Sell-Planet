bcc4347ff45e9a15cffe58cf0c9ff4c69662067a
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-03-15 14:11:47 +0800

    Add files via upload

---
+++ PicSellPlanet/assets/js/agency.js
@@ -0,0 +1,17 @@
+
+    // Hide navbar when modals trigger
+    var modals = document.querySelectorAll('.portfolio-modal');
+      
+    for (var modal of modals) {
+      
+      modal.addEventListener('shown.bs.modal', function (event) {
+        mainNav.classList.add('d-none');
+      });
+        
+      modal.addEventListener('hidden.bs.modal', function (event) {
+        mainNav.classList.remove('d-none');
+      });
+    }
+  }
+
+})(); // End of use strict
